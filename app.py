2fc9e43b18097855913e115214c43123e49db49b
puring <784932562@qq.com>
2019-03-02 18:58:48 +0800

    1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 
@@ -9,4 +9,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port= int(os.environ.get('PORT', 5000)))
