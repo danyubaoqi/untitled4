bb57088e4bc5196534bc69020b79da09b48bf648
puring <784932562@qq.com>
2019-03-06 18:51:54 +0800

    1

--- app.py
+++ app.py
@@ -3,26 +3,22 @@ from flask import request, session
 
 app = Flask(__name__)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.form=="POST":
-        user=request.form["user"]
-        session["user"]=user
-        return render_template("room.html",user=user)
+    if request.form == "POST":
+        user = request.form["user"]
+        session["user"] = user
+        return render_template("room.html", user=user)
     else:
-        if session["user"]:
-            return render_template("room.html",user=session["user"])
-        else:return url_for("index")
-
+        if "user" in session:
+            return render_template("room.html", user=session["user"])
+        else:
+            return render_template("login.html")
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!123123123'
-
-
-@app.route("/index")
-def index():
     return app.send_static_file("index.html")
 
 
